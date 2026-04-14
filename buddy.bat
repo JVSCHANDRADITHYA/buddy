8d20c38a2725b05f8459bccd7f9ce77b8ebb41cd
JVSCHANDRADITHYA <adithya.jvsc@gmail.com>
2026-04-14 22:34:08 +0530

    Added instructions

--- buddy.bat
+++ buddy.bat
@@ -5,5 +5,6 @@ REM
 REM Setup (run once as admin, or add folder to PATH):
 REM   setx PATH "%PATH%;G:\ascii_play"
 REM Then restart your terminal and run: buddy video.mp4
+REM If this does not work use python manual method
 
 python "%~dp0ascii_play\cli.py" %*
